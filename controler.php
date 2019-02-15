6ee307bbc6c0464d2c91a3b57b8a9b94df8427d4
CodeArtha <codeartha@gmail.com>
2019-02-15 10:22:52 -0800

    Lots of bugfixes, now the page loads, text is kept between form posts, encryption don't throw errors, however the cipher text is not correct

--- controler.php
+++ controler.php
@@ -1,58 +1,57 @@
 ﻿<?php
-session_start();
 
-$alpha = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-
-function encrypt($message, $key)
+function encrypt(string $message, string $key): string
 {
-	global $alpha;
+	$result = '';
+	$alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	$msgLength = strlen($message);
 	$keyLength = strlen($key);
 	$alphaLength = count($alpha);
 	$cryptedChar;
 
-	for($i = 0; $i >= $msgLength; $i++)
+	for($i = 0; $i < $msgLength; $i++)
 	{
-		$cryptedChar = $alpha[ cIndex($message[$i]) + cIndex($i % $keyLength) % $alphaLength ];
-		$result = $result . $cryptedChar ;
+		$msgLetterIndex = array_search($message[$i], $alpha);
+		$keyLetterIndex = array_search($key[$i % $keyLength], $alpha);
+		$cryptedChar = $alpha[ ($msgLetterIndex + $keyLetterIndex) % $alphaLength ];
+		$result .= $cryptedChar ;
 	}
-
 	return $result;
 }
 
-function cIndex($par1)
+function decrypt(string $message, string $key): string
 {
-	global $alpha;
-	return array_search($par1, $alpha);
-}
-	
+	$result = '';
+	$alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	$msgLength = strlen($message);
+	$keyLength = strlen($key);
+	$alphaLength = count($alpha);
+	$plainChar;
 
+	for($i = 0; $i < $msgLength; $i++)
+	{
+		$msgLetterIndex = array_search($message[$i], $alpha);
+		$keyLetterIndex = array_search($key[$i % $keyLength], $alpha);
+		$plainChar = $alpha[ ($msgLetterIndex + $keyLetterIndex) % $alphaLength ];
+		$result .= $plainChar ;
+	}
+	return $result;
+}
 
 // Content Validation
-	
-if(isset($_POST['pwd'])) // Password set
-{
-	if(isset($_POST['usr_in']) AND !preg_match("/[^(a-zA-Z 0-9|+|-|*|:|!|?)]/" , htmlspecialchars($_POST['usr_in'])))// Message only contains usable chars
-	{
-		// Main
-		(isset($_POST['remember']) && $_POST['remember']) ? $_SESSION['pwd'] = htmlspecialchars($_POST['pwd']) : session_unset();
-		
-		if($_POST['cmd'] == "hide")
-		{	
-			$_SESSION['story'] = encrypt(htmlspecialchars($_POST['usr_in']), htmlspecialchars($_POST['pwd']));
-			header('Location: gui.php?msg=0');
-			exit();
-		}
-		else if ($_POST['cmd'] == "seek") 
-		{
-			$_SESSION['story'] = $vig->decrypt();
-			header('Location: gui.php?msg=0');
-			exit();
-		}
 
-		// end Main
+if(isset($_POST['pwd']) && isset($_POST['usr_in'])) // Password set
+{
+	if(!preg_match("/[^(a-zA-Z 0-9|+|-|*|:|!|?)]/" , htmlspecialchars($_POST['usr_in'])))// Message only contains usable chars
+	{
+		if($_POST['cmd'] === "hide")
+		{
+			$_POST['usr_in'] = encrypt(htmlspecialchars($_POST['usr_in']), htmlspecialchars($_POST['pwd']));
+		}
+		else if ($_POST['cmd'] === "seek")
+		{
+			$_POST['usr_in'] = decrypt(htmlspecialchars($_POST['usr_in']), htmlspecialchars($_POST['pwd']));
+		}
 	}
-	else{header('Location: gui.php?msg=2'); exit();}
 }
-else{header('Location: gui.php?msg=1'); exit();}
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,38 @@
-<?php
-session_start();
+<?php error_reporting(E_ALL);?>
+<?php include 'controler.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<title>Vigenere</title>
+</head>
 
-header('Location: gui.php');
-exit();
+<body>
+<div class="container">
+	<div class="jumbotron">
+    <h1><u>Vigenere Cipher</u></h1>
+    <p>Uses the old vigenere algorithm.</p>
+  </div>
 
-?>
+	<form role="form" action="index.php" method="POST" >
+		<div class="form-group">
+			<textarea name="usr_in" rows="8" placeholder="Type your text here" class="form-control"><?php if(isset($_POST['usr_in'])){echo $_POST['usr_in'];}?></textarea>
+		</div>
+		<div class="form-group">
+			<input type="password" name="pwd" placeholder="Password" class="form-control"></input>
+		</div>
+		<button type="submit" name="cmd" value="hide">Crypt</button>
+		<button type="submit" name="cmd" value="seek">Decrypt</button>
+	</form>
+
+</div><!-- Closing container -->
+</body>
+
+<footer>
+	<p class="text-center">Copyright - 2015 - <a href="http://keybase.io/codeartha">CodeArtha</p>
+</footer>
+</html>
